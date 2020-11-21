556e213982021b04ca1b9c57d45e58aa55366b4b
ArtistaVeKaras <claudiooartista@hotmail.co.uk>
2020-11-21 14:29:36 +0000

    Added a post request to the views file

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
+
 
 class HelloApiView(APIView):
     """Test API view"""
+    serializer_class = serializers.HelloSerializers
+
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
@@ -13,3 +19,16 @@ class HelloApiView(APIView):
             'Is mapped manually to URLS',
         ]
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a help message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST)
